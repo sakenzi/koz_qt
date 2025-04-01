83f815a6a65c39d7ab05d48522469f6e3d12af03
sakenzi <ermekulysaken8@gmail.com>
2025-04-01 13:41:05 +0600

    был добавлен шаблон комнаты ожидание

--- views/waiting_for_window/wainting_for_window.py
+++ views/waiting_for_window/wainting_for_window.py
@@ -1,29 +1,27 @@
-from PyQt6.QtWidgets import (QVBoxLayout, QWidget, QLabel, QLineEdit, QApplication, QPushButton, QMainWindow)
 import sys
+from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QMainWindow)
 
 
-class ExamWindow(QMainWindow):
+class WaitingForWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Экзамен алаңы")
+        self.setWindowTitle("Күту зонасы")
         self.setGeometry(250, 50, 1000, 700)
         self.setStyleSheet("""
             QMainWindow {
                 background-color: #a3c0f0;
                 border: thick double #32a1ce;
                 border-radius: 20px;
-            }
         """)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         self.layout = QVBoxLayout(self.central_widget)
 
-        
 app = QApplication(sys.argv)
 
-window = ExamWindow()
+window = WaitingForWindow()
 window.show()
 
 app.exec()
---
+++ views/window.py
@@ -0,0 +1,6 @@
+from main_window.main_window import MainWindow
+
+
+class Window(MainWindow):
+    def __init__(self):
+        pass
