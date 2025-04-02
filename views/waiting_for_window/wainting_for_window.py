a4c0a157b43880a5b39a6259d084de30263abb7f
sakenzi <ermekulysaken8@gmail.com>
2025-04-02 15:19:59 +0600

    добавил гифку loading

--- views/waiting_for_window/wainting_for_window.py
+++ views/waiting_for_window/wainting_for_window.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QMainWindow)
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTimer, Qt
+from PyQt5 import QtGui
 
 
 class WaitingForWindow(QMainWindow):
@@ -7,19 +9,37 @@ class WaitingForWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Күту зонасы")
-        self.setGeometry(250, 50, 1000, 700)
+        self.setGeometry(50, 50, 1850, 950)
         self.setStyleSheet("""
             QMainWindow {
-                background-color: #a3c0f0;
+                background-color: #0c1214;
                 border: thick double #32a1ce;
                 border-radius: 20px;
+            }
         """)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
+
         self.layout = QVBoxLayout(self.central_widget)
 
-app = QApplication(sys.argv)
+        self.loading_label = QLabel(self.central_widget)
+        self.loading_gif_path = ("images/ZZ5H.gif")
+        self.loading_gif = QtGui.QMovie(self.loading_gif_path)
+        if not self.loading_gif.isValid():
+            self.loading_label.setText("Ошибка: GIF не найден или поврежден")
+        else:
+            self.loading_label.setMovie(self.loading_gif)
+            self.loading_gif.start()
+
+        self.loading_label.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.loading_label)
+
+        # self.timer = QTimer(self)
+        # self.timer.timeout.connect(self.close)
+        # self.timer.start(5000)
+
+app = QApplication(sys.argv)      
 
 window = WaitingForWindow()
 window.show()
