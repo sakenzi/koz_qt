7868616c77b963f93a3db4d26c30d068c6575c52
sakenzi <ermekulysaken8@gmail.com>
2025-04-02 15:34:00 +0600

    изменил местоположение гифки и уменьшиль размер

--- views/waiting_for_window/wainting_for_window.py
+++ views/waiting_for_window/wainting_for_window.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5 import QtGui
+from PyQt5 import QtCore
 
 
 class WaitingForWindow(QMainWindow):
@@ -18,22 +19,27 @@ class WaitingForWindow(QMainWindow):
             }
         """)
 
-        self.central_widget = QWidget()
+        self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
 
-        self.layout = QVBoxLayout(self.central_widget)
-
         self.loading_label = QLabel(self.central_widget)
-        self.loading_gif_path = ("images/ZZ5H.gif")
+        self.loading_gif_path = "images/ZZ5H.gif"
         self.loading_gif = QtGui.QMovie(self.loading_gif_path)
         if not self.loading_gif.isValid():
             self.loading_label.setText("Ошибка: GIF не найден или поврежден")
         else:
+            self.loading_gif.setScaledSize(QtCore.QSize(100, 100))
             self.loading_label.setMovie(self.loading_gif)
             self.loading_gif.start()
 
         self.loading_label.setAlignment(Qt.AlignCenter)
-        self.layout.addWidget(self.loading_label)
+        
+        window_width = 1850
+        window_height = 500
+        gif_size = 100
+        x = (window_width - gif_size) // 2  
+        y = (window_height - gif_size) // 2 
+        self.loading_label.move(x, y)
 
         # self.timer = QTimer(self)
         # self.timer.timeout.connect(self.close)
