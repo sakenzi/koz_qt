0c8a9662802945f5e962e02d0b040be239eec394
sakenzi <ermekulysaken8@gmail.com>
2025-04-05 08:20:38 +0600

    добавил номер строк на notepad и скролл направо

--- views/exam_window/exam_window.py
+++ views/exam_window/exam_window.py
@@ -3,9 +3,9 @@ from PyQt5.QtCore import QPropertyAnimation, QPoint, QSequentialAnimationGroup
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, 
                              QMainWindow, QLabel, QTextEdit, QDialog,
                              QVBoxLayout, QHBoxLayout, QListWidget, QPlainTextEdit)
-from PyQt5.QtGui import QIcon, QPixmap, QFont, QColor
+from PyQt5.QtGui import QIcon, QPixmap, QFont, QColor, QPainter, QTextFormat
 from PyQt5.QtCore import (QTimer, Qt, QPoint, QSequentialAnimationGroup, 
-                          QPropertyAnimation, QEvent, )
+                          QPropertyAnimation, QEvent, QSize, QRect, )
 import time
 
 
@@ -162,6 +162,110 @@ class FullaImageWindow(QMainWindow):
     def closeEvent(self, event):
         super().closeEvent(event)
 
+class LineNumberArea(QWidget):
+    def __init__(self, editor):
+        super().__init__(editor)
+        self.editor = editor
+
+    def sizeHint(self):
+        return QSize(self.editor.line_number_area_width(), 0)
+
+    def paintEvent(self, event):
+        self.editor.line_number_area_paint_event(event)
+
+class CodeEditor(QPlainTextEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.line_number_area = LineNumberArea(self)
+
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.setLineWrapMode(QPlainTextEdit.NoWrap)
+        
+        self.blockCountChanged.connect(self.update_line_number_area_width)
+        self.updateRequest.connect(self.update_line_number_area)
+        self.cursorPositionChanged.connect(self.highlight_current_line)
+        
+        self.update_line_number_area_width(0)
+        
+        self.setStyleSheet("""
+            QPlainTextEdit {
+                background-color: #343c42;
+                color: white;
+                border-radius: 15px;
+                padding: 12px;
+                font-size: 16px;
+                border: none;
+            }
+        """)
+
+    def line_number_area_width(self):
+        digits = 1
+        max_num = max(1, self.blockCount())
+        while max_num >= 10:
+            max_num //= 10
+            digits += 1
+        space = 3 + self.fontMetrics().horizontalAdvance('9') * digits
+        return space
+
+    def update_line_number_area_width(self, _):
+        self.setViewportMargins(self.line_number_area_width(), 0, 0, 0)
+
+    def update_line_number_area(self, rect, dy):
+        if dy:
+            self.line_number_area.scroll(0, dy)
+        else:
+            self.line_number_area.update(0, rect.y(), self.line_number_area.width(), rect.height())
+        
+        if rect.contains(self.viewport().rect()):
+            self.update_line_number_area_width(0)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        cr = self.contentsRect()
+        self.line_number_area.setGeometry(
+            QRect(cr.left(), cr.top(), self.line_number_area_width(), cr.height())
+        )
+
+    def line_number_area_paint_event(self, event):
+        painter = QPainter(self.line_number_area)
+        painter.fillRect(event.rect(), QColor("#343c42"))
+
+        block = self.firstVisibleBlock()
+        block_number = block.blockNumber()
+        top = self.blockBoundingGeometry(block).translated(self.contentOffset()).top()
+        bottom = top + self.blockBoundingRect(block).height()
+
+        while block.isValid() and top <= event.rect().bottom():
+            if block.isVisible() and bottom >= event.rect().top():
+                number = str(block_number + 1)
+                painter.setPen(Qt.white)
+                painter.drawText(
+                    QPoint(self.line_number_area.width() - 5 - self.fontMetrics().horizontalAdvance(number), 
+                        int(top + self.fontMetrics().height())),
+                    number
+            )
+
+            block = block.next()
+            top = bottom
+            bottom = top + self.blockBoundingRect(block).height()
+            block_number += 1
+
+    def highlight_current_line(self):
+        extra_selections = []
+        
+        if not self.isReadOnly():
+            selection = QTextEdit.ExtraSelection()
+            line_color = QColor("#343c42")
+            
+            selection.format.setBackground(line_color)
+            selection.format.setProperty(QTextFormat.FullWidthSelection, True)
+            selection.cursor = self.textCursor()
+            selection.cursor.clearSelection()
+            extra_selections.append(selection)
+        
+        self.setExtraSelections(extra_selections)
+
 class ExamWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -302,24 +406,8 @@ class ExamWindow(QMainWindow):
         self.image_label.mousePressEvent = self.show_fullscreen_image
         image_text_layout.addWidget(self.image_label)
 
-        self.notepad = QPlainTextEdit()
+        self.notepad = CodeEditor()
         self.notepad.setPlaceholderText("Тапсырманы жазыңыз")
-        self.notepad.setStyleSheet("""
-            QPlainTextEdit {
-                background-color: #343c42;
-                color: white;
-                border-radius: 15px;
-                padding: 12px;
-                font-size: 16px;
-                border: none;
-            }
-            QPlainTextEdit:hover {
-                background-color: #4f565c;
-            }
-            QPlainTextEdit:pressed {
-                background-color: #808a91;
-            }
-        """)
         image_text_layout.addWidget(self.notepad, stretch=1)
 
         exit_layout = QHBoxLayout()
